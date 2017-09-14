abe07bddc83252e4381c218ebf53c9a986865db9
Juan Jorge Garcia <juan.j.garcia007@gmail.com>
2017-09-13 23:59:05 -0300

    Melhorado as responsividade do site para telas maiores, adicionado bordas para o conteudo

--- juan/index.js
+++ juan/index.js
@@ -3,11 +3,8 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
     
     var slider = $('.slider').slider();
-    slider.slider(
-        {
-            height:500,
-            interval:3000,
-        }
-    )
+    slider.slider({        
+        interval:3000,
+    })
       
 });
