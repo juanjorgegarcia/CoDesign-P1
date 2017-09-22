dbd9c0ecdeefb8aaf80909a1309f767a6041d5a3
arthurolga <arthur_olga@hotmail.com>
2017-09-22 00:04:30 -0300

    form de contato funcional

--- juan/index.js
+++ juan/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-<<<<<<< HEAD
     $(".button-collapse").sideNav();
     
     var slider = $('.slider').slider(
@@ -9,20 +8,4 @@ $(document).ready(function() {
     );
     $('.parallax').parallax();  
 });
-=======
-    $(".button-collapse").sideNav({
-        closeOnClick: true,
 
-    });
-    
-    var slider = $('.slider').slider();
-    slider.slider({      
-        interval:3000,
-    })
-    
-    $('.parallax').parallax();
-    $('.modal-trigger').leanModal();
-});
-
-
->>>>>>> 904c93c3a25904dec22827d92e19e0d8ec1e88af
